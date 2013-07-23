73b6d783546e5d0b67f57bde2ecf194831ad6321
jbtule <jay+code@tuley.name>
2013-07-23 02:35:39 -0500

    vagrant box for running tests
    To use:
      - Install Virtual Box: https://www.virtualbox.org/wiki/Downloads
      - Install Vagrant: http://downloads.vagrantup.com/
      - cd make-interop-data
      - vagrant up #first time will download image, provision all software
      - vagrant ssh #logs into the virtual machine
      - cd /vagrant
      - ./checkout-build-test-data.sh -- download sources, build test

---
+++ make-interop-data/bootstrap.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y update
+#first
+apt-get -y install git
+apt-get -y install mercurial
+#cpp
+apt-get -y install make
+apt-get -y install g++
+apt-get -y install scons
+apt-get -y install swig
+# java
+apt-get -y install openjdk-6-jdk
+apt-get -y install maven
+# python
+apt-get -y install python-dev
+apt-get -y install python3-dev
+apt-get -y install python-setuptools
+apt-get -y install python3-setuptools
+# dotnet
+apt-get -y install mono-devel
+mozroots --import --sync
+# go
+apt-get -y install golang
+go get "github.com/jessevdk/go-flags"
+go get "code.google.com/p/go.crypto/pbkdf2"
+#apps
+apt-get -y install vim
+apt-get -y install tig
+
